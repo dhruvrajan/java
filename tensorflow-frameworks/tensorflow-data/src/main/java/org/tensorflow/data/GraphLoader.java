15a80123099d2ce78d390b813a1a116838b7bf14
Dhruv Rajan <dhruv@krishnaprem.com>
2019-11-07 13:32:57 -0800

    Populating tf.data submodule

--- tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/GraphLoader.java
+++ tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/GraphLoader.java
@@ -2,13 +2,10 @@ package org.tensorflow.data;
 
 import org.tensorflow.Operand;
 import org.tensorflow.Session;
-import org.tensorflow.Tensor;
 import org.tensorflow.op.Ops;
-
-import java.util.Iterator;
 
 public interface GraphLoader<T> extends Dataset<T>, AutoCloseable {
   void build(Ops tf);
   Operand<T>[] getBatchOperands();
-  void addBatchToSessionRunner(Ops tf, Session.Runner runner, long batchIndex, boolean fetchBatches);
+  void feedBatchToSessionRunner(Ops tf, Session.Runner runner, long batchIndex, boolean fetchBatches);
 }
