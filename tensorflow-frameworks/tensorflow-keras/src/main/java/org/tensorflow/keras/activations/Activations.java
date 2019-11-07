372349606739dddca74882a5155b8765117be555
Dhruv Rajan <dhruv@krishnaprem.com>
2019-11-07 13:24:56 -0800

    Simplify Activations code

--- tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/activations/Activations.java
+++ tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/activations/Activations.java
@@ -1,11 +1,9 @@
 package org.tensorflow.keras.activations;
 
 import org.tensorflow.Operand;
-import org.tensorflow.keras.mixin.ActivationFunction;
 import org.tensorflow.op.Ops;
-import org.tensorflow.op.math.Sigmoid;
-import org.tensorflow.op.math.Tanh;
-import org.tensorflow.op.nn.*;
+
+import java.util.function.BiFunction;
 
 /**
  * Helper functions to compute activations using a TF Ops object.
@@ -24,82 +22,26 @@ public enum Activations {
     /**
      * Map from `Activations` enumeration to respective activation functions.
      */
-    private static <T extends Number> ActivationFunction<T> getActivationFunction(Activations type) {
+    private static <T extends Number> BiFunction<Ops, Operand<T>, Operand<T>> getActivationFunction(Activations type) {
         switch (type) {
             case linear:
-                return Activations::linear;
+                return (tf, x) -> x;
             case sigmoid:
-                return Activations::sigmoid;
+                return (tf, x) -> tf.math.sigmoid(x);
             case tanh:
-                return Activations::tanh;
+                return (tf, x) -> tf.math.tanh(x);
             case relu:
-                return Activations::relu;
+                return (tf, x) -> tf.nn.relu(x);
             case elu:
-                return Activations::elu;
+                return (tf, x) -> tf.nn.elu(x);
             case selu:
-                return Activations::selu;
+                return (tf, x) -> tf.nn.selu(x);
             case softmax:
-                return Activations::softmax;
+                return (tf, x) -> tf.nn.softmax(x);
             case logSoftmax:
-                return Activations::logSoftmax;
+                return (tf, x) -> tf.nn.logSoftmax(x);
             default:
-                throw new IllegalArgumentException("Invalid Activation Type");
+                throw new IllegalArgumentException("Invalid Activation Type: " + type.name());
         }
-    }
-
-    /**
-     * Linear activation function (no op).
-     */
-    public static <T> Operand<T> linear(Ops tf, Operand<T> x) {
-        return x;
-    }
-
-    /**
-     * Sigmoid activation function.
-     */
-    public static <T> Sigmoid<T> sigmoid(Ops tf, Operand<T> x) {
-        return tf.math.sigmoid(x);
-    }
-
-    /**
-     * Tanh activation function.
-     */
-    public static <T> Tanh<T> tanh(Ops tf, Operand<T> x) {
-        return tf.math.tanh(x);
-    }
-
-    /**
-     * Rectified linear unit.
-     */
-    public static <T> Relu<T> relu(Ops tf, Operand<T> x) {
-        return tf.nn.relu(x);
-    }
-
-    /**
-     * Exponential linear unit.
-     */
-    public static <T extends Number> Elu<T> elu(Ops tf, Operand<T> x) {
-        return tf.nn.elu(x);
-    }
-
-    /**
-     * Scaled exponential linear Unit.
-     */
-    public static <T extends Number> Selu<T> selu(Ops tf, Operand<T> x) {
-        return tf.nn.selu(x);
-    }
-
-    /**
-     * Softmax activation function.
-     */
-    public static <T extends Number> Softmax<T> softmax(Ops tf, Operand<T> x) {
-        return tf.nn.softmax(x);
-    }
-
-    /**
-     * Log Softmax activation function.
-     */
-    public static <T extends Number> LogSoftmax<T> logSoftmax(Ops tf, Operand<T> x) {
-        return tf.nn.logSoftmax(x);
     }
 }
