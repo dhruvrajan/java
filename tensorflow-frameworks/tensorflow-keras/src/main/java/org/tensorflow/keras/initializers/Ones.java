fb26046b32fac8bf7f53a2899038ae78d0dbf733
Dhruv Rajan <dhruv@krishnaprem.com>
2019-12-11 08:47:30 -0800

    working on batching / dataset iterators

--- tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/initializers/Ones.java
+++ tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/initializers/Ones.java
@@ -1,12 +1,12 @@
- package org.tensorflow.keras.initializers;
-
- import org.tensorflow.Operand;
- import org.tensorflow.op.Ops;
- import org.tensorflow.op.core.Constant;
-
- public class Ones extends Initializer {
-  @Override
-  public <T extends Number> Operand<T> initialize(Ops tf, Operand<Integer> shape, Class<T> dtype) {
-    return tf.fill(shape, Constant.create(tf.scope(),1.0f, dtype));
-  }
- }
+// package org.tensorflow.keras.initializers;
+//
+// import org.tensorflow.Operand;
+// import org.tensorflow.op.Ops;
+// import org.tensorflow.op.core.Constant;
+//
+// public class Ones extends Initializer {
+//  @Override
+//  public <T extends Number> Operand<T> initialize(Ops tf, Operand<Integer> shape, Class<T> dtype) {
+//    return tf.fill(shape, Constant.create(tf.scope(),1.0f, dtype));
+//  }
+// }
--- tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/layers/Flatten.java
+++ tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/layers/Flatten.java
@@ -1,37 +1,37 @@
-package org.tensorflow.keras.layers;
+// package org.tensorflow.keras.layers;
 
-import org.tensorflow.Operand;
-import org.tensorflow.Shape;
-import org.tensorflow.keras.utils.TensorShape;
-import org.tensorflow.op.Ops;
-import org.tensorflow.op.core.Constant;
+// import org.tensorflow.Operand;
+// import org.tensorflow.Shape;
+// import org.tensorflow.keras.utils.TensorShape;
+// import org.tensorflow.op.Ops;
+// import org.tensorflow.op.core.Constant;
 
-public class Flatten<T extends Number> extends Layer<T> {
-    private static int FLATTEN_INPUT_LENGTH = 1;
-    private Constant<Integer> units;
+// public class Flatten<T extends Number> extends Layer<T> {
+//     private static int FLATTEN_INPUT_LENGTH = 1;
+//     private Constant<Integer> units;
 
-    public Flatten() {
-        super(FLATTEN_INPUT_LENGTH);
-    }
+//     public Flatten() {
+//         super(FLATTEN_INPUT_LENGTH);
+//     }
 
-    @Override
-    public void build(Ops tf, Shape inputShape) {
-        TensorShape tensorShape = new TensorShape(inputShape);
-        this.units = Constant.create(tf.scope(),new int[] {-1, (int) (tensorShape.numElements() / Math.abs(tensorShape.size(0)))});
-    }
+//     @Override
+//     public void build(Ops tf, Shape inputShape) {
+//         TensorShape tensorShape = new TensorShape(inputShape);
+//         this.units = Constant.create(tf.scope(),new int[] {-1, (int) (tensorShape.numElements() / Math.abs(tensorShape.size(0)))});
+//     }
 
-    public Shape computeOutputShape(Shape inputShape) {
-        // leaves unknown dimensions unknown
-        return Shape.make(new TensorShape(inputShape).numElements());
-    }
+//     public Shape computeOutputShape(Shape inputShape) {
+//         // leaves unknown dimensions unknown
+//         return Shape.make(new TensorShape(inputShape).numElements());
+//     }
 
-    @SafeVarargs
-    public final Operand<T> call(Ops tf, Operand<T>... inputs) {
-        return this.call(tf, inputs[0]);
-    }
+//     @SafeVarargs
+//     public final Operand<T> call(Ops tf, Operand<T>... inputs) {
+//         return this.call(tf, inputs[0]);
+//     }
 
-    private Operand<T> call(Ops tf, Operand<T> input) {
-        return tf.reshape(input, this.units);
-    }
+//     private Operand<T> call(Ops tf, Operand<T> input) {
+//         return tf.reshape(input, this.units);
+//     }
 
-}
+// }
