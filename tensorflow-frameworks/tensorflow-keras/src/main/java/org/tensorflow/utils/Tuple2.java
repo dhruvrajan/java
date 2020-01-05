b2addc789be2de0ef6c6ba82c6a79ab08898a9e1
Dhruv Rajan <dhruv@krishnaprem.com>
2020-01-05 07:04:43 -0800

    working on keras

--- tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/utils/Tuple2.java
+++
@@ -1,9 +0,0 @@
-package org.tensorflow.utils;
-
-import org.tensorflow.data.Pair;
-
-public class Tuple2<T> extends Pair<T, T> {
-    public Tuple2(T first, T second){
-        super(first, second);
-    }
-}
