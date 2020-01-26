b95a0d81249e1d26a6aaeb524ea12603e97be023
Dhruv Rajan <dhruv@krishnaprem.com>
2020-01-26 00:49:54 -0800

    verified that graph mode batching during training works in tf.data

--- tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/examples/mnist/MNISTClassifier.java
+++ tensorflow-frameworks/tensorflow-keras/src/main/java/org/tensorflow/keras/examples/mnist/MNISTClassifier.java
@@ -17,12 +17,9 @@ import org.tensorflow.op.core.Assign;
 import org.tensorflow.op.core.Constant;
 import org.tensorflow.op.core.Gradients;
 import org.tensorflow.op.core.Variable;
-import org.tensorflow.op.math.Mean;
-import org.tensorflow.op.nn.Softmax;
 import org.tensorflow.op.train.ApplyGradientDescent;
 import org.tensorflow.tools.Shape;
 import org.tensorflow.types.TFloat32;
-import org.tensorflow.types.TInt32;
 import org.tensorflow.types.TInt64;
 import org.tensorflow.utils.Pair;
 
@@ -32,10 +29,41 @@ public class MNISTClassifier implements Runnable {
     private static final float LEARNING_RATE = 0.2f;
     private static final int FEATURES = 10;
     private static final int BATCH_SIZE = 100;
-    private static final int EPOCHS = 50;
+    private static final int EPOCHS = 10;
 
     public static void main(String[] args) {
         new MNISTClassifier().run();
+    }
+
+    public Operand<TFloat32> predict(Ops tf, Operand<TFloat32> images, Variable<TFloat32> weights,
+            Variable<TFloat32> biases) {
+        return tf.nn.softmax(tf.math.add(tf.linalg.matMul(images, weights), biases));
+    }
+
+    public Operand<TFloat32> crossEntropyLoss(Ops tf, Operand<TFloat32> predicted, Operand<TFloat32> actual) {
+        return tf.math.mean(tf.math.neg(tf.reduceSum(tf.math.mul(actual, tf.math.log(predicted)), tf.constant(1))),
+                tf.constant(0));
+    }
+
+    public List<ApplyGradientDescent<TFloat32>> optimizerTargetOps(Ops tf, Operand<TFloat32> loss,
+            Operand<TFloat32> weights, Operand<TFloat32> biases) {
+        Gradients gradients = tf.gradients(loss, Arrays.asList(weights, biases));
+        Constant<TFloat32> alpha = tf.constant(LEARNING_RATE);
+
+        ApplyGradientDescent<TFloat32> weightGradientDescent = tf.train.applyGradientDescent(weights, alpha,
+                gradients.dy(0));
+        ApplyGradientDescent<TFloat32> biasGradientDescent = tf.train.applyGradientDescent(biases, alpha,
+                gradients.dy(1));
+
+        return Arrays.asList(weightGradientDescent, biasGradientDescent);
+    }
+
+    public Operand<TFloat32> accuracy(Ops tf, Operand<TFloat32> predicted, Operand<TFloat32> actual) {
+        Operand<TInt64> yHat = tf.math.argMax(predicted, tf.constant(1));
+        Operand<TInt64> yTrue = tf.math.argMax(actual, tf.constant(1));
+        Operand<TFloat32> accuracy = tf.math.mean(tf.dtypes.cast(tf.math.equal(yHat, yTrue), TFloat32.DTYPE),
+                tf.constant(0));
+        return accuracy;
     }
 
     public void run() {
@@ -53,63 +81,60 @@ public class MNISTClassifier implements Runnable {
             Dataset trainDataset = trainTestSplit.first().batch(BATCH_SIZE);
             Dataset testDataset = trainTestSplit.second().batch(BATCH_SIZE);
 
-            // Graph mode iteration with OneShotIterator
+            // Extract iterators and train / test components
             OneShotIterator trainIterator = trainDataset.makeOneShotIterator();
             List<Output<?>> components = trainIterator.getComponents();
-            Operand<TFloat32> images = components.get(0).expect(TFloat32.DTYPE);
-            images = tf.reshape(images, tf.constant(new int[] {-1, 28 * 28}));
-            Operand<TFloat32> labels = components.get(1).expect(TFloat32.DTYPE);
+            Operand<TFloat32> trainImages = components.get(0).expect(TFloat32.DTYPE);
+            Operand<TFloat32> trainLabels = components.get(1).expect(TFloat32.DTYPE);
+
+            OneShotIterator testIterator = testDataset.makeOneShotIterator();
+            List<Output<?>> testComponents = testIterator.getComponents();
+            Operand<TFloat32> testImages = testComponents.get(0).expect(TFloat32.DTYPE);
+            Operand<TFloat32> testLabels = testComponents.get(1).expect(TFloat32.DTYPE);
+
+            // Flatten image tensors
+            trainImages = tf.reshape(trainImages, tf.constant(new int[] { -1, 28 * 28 }));
+            testImages = tf.reshape(testImages, tf.constant(new int[] { -1, 28 * 28 }));
 
             // Declare, initialize weights
             Variable<TFloat32> weights = tf.variable(Shape.make(INPUT_SIZE, FEATURES), TFloat32.DTYPE);
             Assign<TFloat32> weightsInit = tf.assign(weights,
-                    tf.zeros(constArray(tf, INPUT_SIZE, FEATURES), TFloat32.DTYPE));
+                    tf.zeros(tf.constant(new int[] { INPUT_SIZE, FEATURES }), TFloat32.DTYPE));
 
             Variable<TFloat32> biases = tf.variable(Shape.make(FEATURES), TFloat32.DTYPE);
-            Assign<TFloat32> biasesInit = tf.assign(biases, tf.zeros(constArray(tf, FEATURES), TFloat32.DTYPE));
+            Assign<TFloat32> biasesInit = tf.assign(biases,
+                    tf.zeros(tf.constant(new int[] { FEATURES }), TFloat32.DTYPE));
 
-            // 'Forward' call
-            Softmax<TFloat32> softmax = tf.nn.softmax(tf.math.add(tf.linalg.matMul(images, weights), biases));
-            Mean<TFloat32> crossEntropy = tf.math.mean(
-                    tf.math.neg(tf.reduceSum(tf.math.mul(labels, tf.math.log(softmax)), constArray(tf, 1))),
-                    constArray(tf, 0));
+            // SETUP: Training
+            Operand<TFloat32> trainPrediction = predict(tf, trainImages, weights, biases);
+            Operand<TFloat32> trainAccuracy = accuracy(tf, trainPrediction, trainLabels);
 
-            // Gradient optimizer step
-            Gradients gradients = tf.gradients(crossEntropy, Arrays.asList(weights, biases));
-            Constant<TFloat32> alpha = tf.constant(LEARNING_RATE);
-            ApplyGradientDescent<TFloat32> weightGradientDescent = tf.train.applyGradientDescent(weights, alpha,
-                    gradients.dy(0));
-            ApplyGradientDescent<TFloat32> biasGradientDescent = tf.train.applyGradientDescent(biases, alpha,
-                    gradients.dy(1));
+            Operand<TFloat32> trainLoss = crossEntropyLoss(tf, trainPrediction, trainLabels);
+            List<ApplyGradientDescent<TFloat32>> optimizerTargets = optimizerTargetOps(tf, trainLoss, weights, biases);
 
-            // Compute accuracy metric
-            Operand<TInt64> predicted = tf.math.argMax(softmax, tf.constant(1));
-            Operand<TInt64> expected = tf.math.argMax(labels, tf.constant(1));
-            Operand<TFloat32> accuracy = tf.math
-                    .mean(tf.dtypes.cast(tf.math.equal(predicted, expected), TFloat32.DTYPE), tf.constant(0));
+            // SETUP: Testing
+            Operand<TFloat32> testPrediction = predict(tf, testImages, weights, biases);
+            Operand<TFloat32> testAccuracy = accuracy(tf, testPrediction, testLabels);
 
             try (Session session = new Session(graph)) {
 
-                // Create iterator object
-                session.runner()
-                    .addTarget(weightsInit)
-                    .addTarget(biasesInit)
-                    .run();
+                // Initialize weights and biases
+                session.runner().addTarget(weightsInit).addTarget(biasesInit).run();
 
+                // Run training loop
                 for (int i = 0; i < EPOCHS; i++) {
                     // reset iterator object
                     session.runner().addTarget(trainIterator.getMakeIteratorOp()).run();
-                    
+
                     int batches = 0;
                     float epochAccuracy = 0;
                     while (true) {
                         try {
                             List<Tensor<?>> fetched = session.runner()
-                                .addTarget(images)
-                                .addTarget(labels)
-                                .addTarget(weightGradientDescent)
-                                .addTarget(biasGradientDescent)
-                                .fetch(accuracy).run();
+                                .addTarget(optimizerTargets.get(0))
+                                .addTarget(optimizerTargets.get(1))
+                                .fetch(trainAccuracy)
+                                .run();
 
                             float batchAccuracy = fetched.get(0).floatValue();
                             epochAccuracy += batchAccuracy;
@@ -121,11 +146,27 @@ public class MNISTClassifier implements Runnable {
 
                     System.out.println("Epoch Accuracy " + i + ": " + epochAccuracy / batches);
                 }
+
+                // Evaluate on test set
+                session.runner().addTarget(testIterator.getMakeIteratorOp()).run();
+
+                int batches = 0;
+                float epochAccuracy = 0;
+                while (true) {
+                    try {
+                        List<Tensor<?>> fetched = session.runner().fetch(testAccuracy).run();
+
+                        float batchAccuracy = fetched.get(0).floatValue();
+                        epochAccuracy += batchAccuracy;
+                        batches += 1;
+                    } catch (IndexOutOfBoundsException e) {
+                        break;
+                    }
+                }
+
+                System.out.println("Test Accuracy " + ": " + epochAccuracy / batches);
+
             }
         }
-    }
-
-    private static Operand<TInt32> constArray(Ops tf, int... i) {
-        return tf.constant(i);
     }
 }
